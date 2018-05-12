49339399791b2875594cc5196d97baadfba24d0a
Yung chieh Tsai <x01ep23i@hotmail.com>
2018-05-12 14:01:19 +0800

    solve conflict

--- src/js/background.js
+++ src/js/background.js
@@ -1,24 +1,4 @@
 const resolver = require('./resolver.js');
-<<<<<<< HEAD
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-  let name = details.url.substring(7, details.url.length - 1)
-  chrome.tabs.getSelected(null, (tab) => {
-    chrome.tabs.update(tab.id, {url: "loading.html"})
-    resolver.resolve(name).then((ipfsHash) => {
-      console.log("IPFS hash for " + name + ": " + ipfsHash)
-      let url = "https://gateway.ipfs.io/ipfs/" + ipfsHash
-      return fetch(url, {method: "HEAD"})
-      .then((response) => response.status)
-      .then((statusCode) => {
-        if (statusCode === 200) {
-          console.log("Serving content from local IPFS gateway")
-          chrome.tabs.update(tab.id, {url: url})
-        }
-        return "Gateway"
-      })
-      .catch((err) => {
-        console.log("Could not find local IPFS gateway so serving content from public IPFS gateway instead")
-=======
 const extension = require('extensionizer');
 extension.webRequest.onBeforeRequest.addListener(details => {
   let name = details.url.substring(7, details.url.length - 1)
@@ -38,21 +18,14 @@ extension.webRequest.onBeforeRequest.addListener(details => {
         extension.tabs.update(tab.id, {url: url})
       })
       .catch(err => {
->>>>>>> 54545b32ea580b510ab1d654dd5f67225024240c
         url = "https://gateway.ipfs.io/ipfs/" + ipfsHash
         extension.tabs.update(tab.id, {url: url})
         return err
       })
     })
-<<<<<<< HEAD
-    .catch((err) => {
-      var nameWithoutTld = name.substring(0, name.lastIndexOf('.'))
-      chrome.tabs.update(tab.id, {url: "error.html?name=" + name})
-=======
     .catch(err => {
       let nameWithoutTld = name.substring(0, name.lastIndexOf('.'))
       extension.tabs.update(tab.id, {url: "error.html?name=" + name})
->>>>>>> 54545b32ea580b510ab1d654dd5f67225024240c
     })
   })
   return {cancel: true}
